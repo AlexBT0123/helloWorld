6c21a470a0fd5810ffa49dd2f58944cfec81c1f6
Alex Taylor <alexander.taylor7@snhu.edu>
2023-09-21 11:31:08 -0400

    A

--- pages/urls.py
+++ pages/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("pages.urls")), # new
+    path("about/", AboutPageView.as_view(), name="about"), # new
+    path("", HomePageView.as_view(), name="home"),
 ]
+
